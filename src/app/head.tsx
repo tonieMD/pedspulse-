874c8c7f9eda97487f58ade83f9c84d275e7a2f9
tonieMD <mbumarash1@gmail.com>
2025-05-21 01:37:16 +0300

    changed the title

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>PedsPulse Evidence-based Pediatrics for Junior Docs</title>
+      <meta
+        name="description"
+        content="Join Dr. Rashid, MD for bite-size clinical pearls, cases & quizzes."
+      />
+    </>
+  )
+}
